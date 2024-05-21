6e6f8dcbd06ae496a2fc0eefda2bcf3371c519b4
Marcelo Tanaka <marcetanakasato@gmail.com>
2024-05-20 20:46:40 -0500

    add: login and logout

--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .forms import CustomAuthentificationForm
+from django.contrib.auth.views import LoginView
 
 def home(request):
     return render(request, "core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthentificationForm
+    template_name = "core/login.html"
