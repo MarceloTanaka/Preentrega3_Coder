3745ea3af8e73136967afcd4dbafa2bf9c1cc0f7
Marcelo Tanaka <marcetanakasato@gmail.com>
2024-05-24 18:01:13 -0500

    add: CRUD producto

--- project/Producto/urls.py
+++ project/Producto/urls.py
@@ -6,4 +6,7 @@ app_name = "Producto"
 urlpatterns = [
     path("", views.home, name="home"),
     path("agregarproducto/", views.agregarproducto, name="agregarproducto"),
+    path("productodetail/<int:pk>", views.producto_detail, name="producto_detail"),
+    path("productoupdate/<int:pk>", views.producto_update, name="producto_update"),
+    path("productodelete/<int:pk>", views.producto_delete, name="producto_delete")
 ]
--- project/Producto/views.py
+++ project/Producto/views.py
@@ -17,4 +17,24 @@ def agregarproducto(request):
         form = forms.ProductoForm()
     return render(request, "Producto/agregarproducto.html", context={"form": form})
 
+def producto_detail(request, pk):
+    query = models.Producto.objects.get(id=pk)
+    return render(request, "Producto/producto_detail.html", {"producto": query})
 
+def producto_update(request, pk: int):
+    query = models.Producto.objects.get(id=pk)
+    if request.method == "POST":
+        form = forms.ProductoForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            return redirect("Producto:home")
+    else:
+        form = forms.ProductoForm(instance=query)
+    return render(request, "Producto/producto_update.html", context={"form": form})
+
+def producto_delete(request, pk: int):
+    query = models.Producto.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("Producto:home")
+    return render(request, "Producto/producto_delete.html", context={"producto": query})
