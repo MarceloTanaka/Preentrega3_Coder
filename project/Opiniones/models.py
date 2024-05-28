3a4e6d91604e627b66aeef8943cd63d2e3a21409
Marcelo Tanaka <marcetanakasato@gmail.com>
2024-05-28 18:05:00 -0500

    add: Login Logout y añadir diseño

--- project/Opiniones/models.py
+++ project/Opiniones/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class Opinion(models.Model):
     titulo = models.CharField(max_length=200)
     reseña = models.CharField(max_length=100000000)
+    fecha_actualizacion = models.DateField(
+        null=True, blank=True, default=timezone.now, editable=False, verbose_name="fecha de actualización"
+    )
 
     def __str__(self):
         return self.titulo 
--- project/core/urls.py
+++ project/core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
