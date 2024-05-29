2ba59d231c4cc99ab5ca3cdea45302d3f6e683c1
Marcelo Tanaka <marcetanakasato@gmail.com>
2024-05-29 14:54:38 -0500

    add: register

--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .forms import CustomAuthentificationForm
+from .forms import CustomAuthentificationForm, CustomUserCreationForm
 from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
 
 def home(request):
     return render(request, "core/index.html")
@@ -8,3 +9,14 @@ def home(request):
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthentificationForm
     template_name = "core/login.html"
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request, "core/index.html", {"mensaje": "Usuario creado"})
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "core/register.html", {"form": form})
