0fe97ea731b0b23d9a4912de78b556f86f299e7e
Marcelo Tanaka <marcetanakasato@gmail.com>
2024-05-31 12:36:51 -0500

    add CRUD in Cliente

--- project/Cliente/urls.py
+++ project/Cliente/urls.py
@@ -5,5 +5,8 @@ app_name = "Cliente"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("cliente/crearcliente", views.crearclientes, name="crearclientes")
+    path("cliente/crearcliente", views.cliente_create, name="cliente_create"),
+    path("cliente/cliente_delete/ <int:pk>", views.ClienteDelete.as_view(), name="cliente_delete"),
+    path("cliente/cliente_detail/ <int:pk>", views.ClienteDetail.as_view(), name="cliente_detail"),
+    path("cliente/cliente_update/ <int:pk>", views.ClienteUpdate.as_view(), name="cliente_update"),
 ]
--- project/Cliente/views.py
+++ project/Cliente/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
+from django.urls import reverse_lazy
 
 def home(request):
     query = models.Cliente.objects.all()
     context = {"clientes": query}
     return render(request, "Cliente/index.html", context)
 
-def crearclientes(request):
+def cliente_create(request):
     if request.method == "POST":
         form = forms.ClienteForm(request.POST)
         if form.is_valid():
@@ -15,4 +16,19 @@ def crearclientes(request):
             return redirect("Cliente:home")
     else:
         form = forms.ClienteForm()
-    return render(request, "Cliente/crearcliente.html", context={"form": form})
+    return render(request, "Cliente/cliente_create.html", context={"form": form})
+
+class ClienteDelete(DeleteView):
+    model = models.Cliente
+    template_name = "Cliente/cliente_delete.html"
+    success_url = reverse_lazy("Cliente:home")
+
+class ClienteDetail(DetailView):
+    model = models.Cliente
+    context_object_name = "cliente"
+
+class ClienteUpdate(UpdateView):
+    model = models.Cliente
+    template_name = "Cliente/cliente_update.html"
+    form_class = forms.ClienteForm
+    success_url = reverse_lazy("Cliente:home")
